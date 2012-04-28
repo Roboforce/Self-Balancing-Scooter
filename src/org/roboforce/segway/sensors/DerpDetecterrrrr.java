f1e67c6e63f251cc27ac9a2cc60fdbd8bd761968
psoloninka <psoloninka@dzone.com>
2012-04-28 11:47:41 -0400

    Added demo file

---
+++ src/org/roboforce/segway/sensors/DerpDetecterrrrr.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.roboforce.segway.sensors;
+
+/**
+ *
+ * @author peter
+ */
+public class DerpDetecterrrrr {
+    public static void main(String[] args){
+        System.out.println("derrrrr");
+    }
+    
+}
